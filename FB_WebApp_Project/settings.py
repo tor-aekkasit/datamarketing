d396a4fe9afda047ba13e37b035c38652e87f76b
ToR_Aekkasit <aekkasit.se@gmail.com>
2025-06-18 18:37:43 +0700

    Update settings.py

--- FB_WebApp_Project/settings.py
+++ FB_WebApp_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-v4a8b%d*kuz4n@*+vp8l1z5a^g*r)hf)kav=i%ju+nzum@9!@)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
